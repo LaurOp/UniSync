b332d674a6285a679338610ad19eb208ee390d0e
LaurGen <oprealaur@yahoo.com>
2024-04-24 13:45:40 +0300

    JWT auth, still two user entities

---
+++ src/main/java/com/example/unisync/Config/Auth/AuthController.java
@@ -0,0 +1,53 @@
+package com.example.unisync.Config.Auth;
+
+import com.example.unisync.Model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserInfoService service;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/user/profile")
+    @PreAuthorize("hasAuthority('USER')")
+    public String userProfile() {
+        return "Welcome to User Profile";
+    }
+
+    @GetMapping("/user/admin")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String adminProfile() {
+        return "Welcome to Admin Profile";
+    }
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
+
+}
+
---
+++ src/main/java/com/example/unisync/Config/Auth/UserInfoRepository.java
@@ -0,0 +1,13 @@
+package com.example.unisync.Config.Auth;
+
+import com.example.unisync.Model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer>
+{
+    Optional<UserInfo> findByName(String username);
+}
